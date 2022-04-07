2509d8fda3aa67fd805f67bbcd974d5e937fa9c3
Tjquillero <69994868+Tjquillero@users.noreply.github.com>
2022-04-06 23:10:05 -0500

    Se agrega el Proyecto

---
+++ mi-new-app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+import Componente from "./components/Componente";
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Boostrap</h1>
+      <Button variant="primary">Click</Button>{' '}
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/HolaMundo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function HolaMundo() {
+    return(
+        <div>
+            <h1>Hola Mundo</h1>
+            <h2>Tomás Javier Herrera</h2>
+        </div>
+    )
+}
+
